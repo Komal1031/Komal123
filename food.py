d8bd0254fcc9878ee6a7bd4d6cb87c527710699c
Komal Jadhwar <89376706+Komal1031@users.noreply.github.com>
2021-10-02 18:26:02 +0530

    Add files via upload

---
+++ food.py
@@ -0,0 +1,25 @@
+[
+{
+    "id": "1",
+    "food":"misal",
+    "recipe":"https://madhurasrecipe.com/veg/misal-pav-latest/",
+    "speciality of":"kolhapur"
+},
+{
+   "id":"2" ,
+   "food":"vadapav",
+   "recipe":"https://www.youtube.com/results?search_query=vadapav+recipe+by+madhura+",
+   "speciality of":"mumbai"
+},
+{
+    "id":"3",
+    "food":"panipuri",
+    "recipe":"https://www.youtube.com/results?search_query=panipuri+pani+recipe+by+madhura",
+    "spciality of":"mumbai"
+},
+{"id":"4",
+"food":"puranpoli",
+"recipe":"https://www.youtube.com/results?search_query=puranpoli+pani+recipe+by+madhura",
+"spciality of":"pune"
+}
+]
